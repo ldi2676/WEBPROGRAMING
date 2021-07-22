0b411d848c5661e65f6b998167a2b85078765e4b
ldi2676 <jw2676@naver.com>
2021-07-22 16:56:12 +0900

    update

--- test.js
+++ test.js
@@ -1,35 +1,7 @@
-/*   window.onscroll = function() {
-  myFunction()
-   };
-   
-   function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight; 
-  var height2 = document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-  
-  alert(height)
-   }   */
-window.addEventListener('DOMContentLoaded', function(){
-    //스크롤 이벤트 처리
-    window.addEventListener("scroll", function(event){
-        if(document.querySelector('.progress-bar') != null)setProgress();
-    });
-});
-$(window).scroll(function() {
-
-    var wins = $(this).scrollTop();
-    // var wins = document.documentElement.scrollTop;
-    var hei = $(".content").outerHeight();
-    var hei2 = $(window).outerHeight();
-    // var hei = document.documentElement.scrollHeight;
-    // var hei2 = document.documentElement.clientHeight;
-    
-    var height = hei - hei2;
-    var bar = (wins / height) * 100;
-    $('#myBar').css('width', bar + '%');
-    // document.querySelector(".myBar").style.width = percentage + "%";//프로그래스바 너비 변경
-  
-});
-  
+var _showPage = function() {
+    var loader = $("div.loader");
+    var container = $("div.container");
+    loader.css("display","none");
+    container.css("display","block");
+        window.location.herf="https://www.naver.com/"
+    };
